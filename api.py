21a4ebfa44933f5071f328aea67935f3d2e0662a
niklascp <niklascp@gmail.com>
2019-07-03 14:44:19 +0200

    Add train

--- api.py
+++ api.py
@@ -12,12 +12,23 @@ def startup():
     atexit.register(event_queue.stop)
 
 @app.route('/link.predict')
-def index():
+def link_predict():
     result = event_queue.call_service(
         service_name = 'link.predict',
         service_data = {
             'linkRef': request.args.get('link_ref'),
-            'model': request.args.get('link_ref', default = 'svr'),
+            'model': request.args.get('model', default = 'svr'),
+            'time': request.args.get('time')
+    })
+    return jsonify(result)
+
+@app.route('/link.train')
+def link_predict():
+    result = event_queue.call_service(
+        service_name = 'link.train',
+        service_data = {
+            'linkRef': request.args.get('link_ref'),
+            'model': request.args.get('model', default = 'svr'),
             'time': request.args.get('time')
     })
     return jsonify(result)
