39cafdaef1cb95d3953d42541cdddd9b0d144bdc
niklascp <niklascp@gmail.com>
2019-07-04 12:50:22 +0200

    Resolve #1

--- api.py
+++ api.py
@@ -1,20 +1,27 @@
 import atexit
 from flask import Flask, jsonify, request
 
-from vehicletracker.azure.events import EventHubsQueue
+from vehicletracker.azure.services import ServiceQueue
 
 app = Flask(__name__)
-event_queue = EventHubsQueue(consumer_group = '$default')
+event_queue = ServiceQueue(domain = 'api')
 
 @app.before_first_request
 def startup():
     event_queue.start()
     atexit.register(event_queue.stop)
 
-@app.route('/link.predict')
+@app.route('/link/models')
+def list_link_models():
+    result = event_queue.call_service(
+        service_name = 'list_link_models',
+        service_data = None)
+    return jsonify(result)
+
+@app.route('/link/predict')
 def link_predict():
     result = event_queue.call_service(
-        service_name = 'link.predict',
+        service_name = 'predict_link',
         service_data = {
             'linkRef': request.args.get('link_ref'),
             'model': request.args.get('model', default = 'svr'),
@@ -22,10 +29,10 @@ def link_predict():
     })
     return jsonify(result)
 
-@app.route('/link.train')
+@app.route('/link/train')
 def link_train():
     result = event_queue.call_service(
-        service_name = 'link.train',
+        service_name = 'schedule_train_link_model',
         service_data = {
             'linkRef': request.args.get('link_ref'),
             'model': request.args.get('model', default = 'svr'),
